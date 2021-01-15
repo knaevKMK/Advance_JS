3be4349db8f93ff11e95b2e22a5bbcaa5a3cc2ed
Krasimir Kanev <knev@mail.bg>
2021-01-15 17:04:23 +0200

    HomeWork

--- Lab_Array/EX_6 Bigger Half.js
+++ Lab_Array/EX_6 Bigger Half.js
@@ -1,3 +1,6 @@
-function solve() {
-    
+function solve(array) {
+    return array.sort((a,b)=>a-b).slice(-1* Math.round(array.length/2));
 }
+
+console.log(solve([4, 7, 2, 5]));
+console.log((solve([3, 19, 14, 7, 2, 19, 6])));
