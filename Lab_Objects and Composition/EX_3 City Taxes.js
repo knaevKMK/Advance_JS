669b395aa8a5ed2b8e6b1107ddc7a0e4349c4a62
Krasimir Kanev <knev@mail.bg>
2021-01-18 22:28:44 +0200

    Practice

--- Lab_Objects and Composition/EX_3 City Taxes.js
+++ Lab_Objects and Composition/EX_3 City Taxes.js
@@ -1,13 +1,34 @@
-function solve(name, population,treasury) {
-    const towns={
-        name: name,
-        population: population,
-        treasury: treasury,
+function collectTaxes(name, population, treasury) {
+    const city = {
+        name,
+        population,
+        treasury,
         taxRate: 10,
-        collectTax: [Function: collectTax],
-    applyGrowth: [Function: applyGrowth],
-    applyRecession: [Function: applyRecession]
-
-
-};
+        collectTaxes() {
+            this.treasury += (this.population * this.taxRate);
+        },
+        applyGrowth(percent) {
+            this.population += Math.floor(this.population * percent / 100)
+        },
+        applyRecession(percent) {
+            this.treasury -= Math.ceil(this.treasury * percent / 100);
+        }
+    };
+    return city;
 }
+
+const city = collectTaxes('Tortuga',
+    7000,
+    15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
+
+
+// console.log(solve('Tortuga',
+//     7000,
+//     15000
+// ));
+
+
