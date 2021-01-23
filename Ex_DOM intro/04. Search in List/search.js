1ad409fb33258a1ed96e096d6b215a7752abaccc
Kr_Knev <kneav@students.softuni.bg>
2021-01-23 23:14:18 +0200

    Update search.js

--- Ex_DOM intro/04. Search in List/search.js
+++ Ex_DOM intro/04. Search in List/search.js
@@ -1,4 +1,18 @@
 function search() {
-  let loolIn= document.getElementById('towns').children;
-    console.log(loolIn);
+  // let lookIn= document.getElementById('towns').children;
+  let lookIn = document.querySelectorAll('ul > li');
+  let serchRegex = document.querySelector('input').value;
+  let matches = 0;
+  lookIn.forEach(li => {
+    if (li.textContent.toLowerCase().includes(serchRegex.toLowerCase())
+      && serchRegex !== '') {
+      matches++;
+      li.style.fontWeight = 'bold';
+      li.style.textDecoration = 'underline';
+    }else{
+      li.style.fontWeight='';
+      li.style.textDecoration='';
+    }
+  });
+  document.querySelector('div').textContent = `${matches} matches found`;
 }
