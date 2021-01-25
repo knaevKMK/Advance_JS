bc1a244de000c741c15044f3b138212fc33dd83c
Kr_Knev <kneav@students.softuni.bg>
2021-01-25 11:26:08 +0200

    Practice

--- Ex_DOM intro/04. Search in List/search.js
+++ Ex_DOM intro/04. Search in List/search.js
@@ -3,16 +3,16 @@ function search() {
   let lookIn = document.querySelectorAll('ul > li');
   let serchRegex = document.querySelector('input').value;
   let matches = 0;
-  lookIn.forEach(li => {
+  for (const li of lookIn) {
     if (li.textContent.toLowerCase().includes(serchRegex.toLowerCase())
       && serchRegex !== '') {
       matches++;
       li.style.fontWeight = 'bold';
       li.style.textDecoration = 'underline';
-    }else{
-      li.style.fontWeight='';
-      li.style.textDecoration='';
+    } else {
+      li.style.fontWeight = '';
+      li.style.textDecoration = '';
     }
-  });
+  }
   document.querySelector('div').textContent = `${matches} matches found`;
 }
--- Ex_DOM intro/05. Table - Serach Engine/solution.js
+++ Ex_DOM intro/05. Table - Serach Engine/solution.js
@@ -1,8 +1,24 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   var lookIn = document.querySelector('tbody').children;
 
    function onClick() {
-      //   TODO:
+      let regex = document.querySelector('#searchField').value.toLowerCase();
+    for (let index = 0; index < lookIn.length; index++) {
+         const rowCells = lookIn[index].children;
+         for (const cell of rowCells) {
+            if (cell.textContent.includes(regex) && regex.trim()!='') {
+               lookIn[index].setAttribute('class' , 'select');
+               break;
+            } else {
+               lookIn[index].removeAttribute('class');
+            }
+         }
+      }
 
+     
+      document.querySelector('#searchField').value = null;
+   //   document.getElementById('result').innerText=
    }
+
 }
