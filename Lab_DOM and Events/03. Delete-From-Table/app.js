240fe0c6f4beeadb90e03e2394de08bbee4b373d
Kr_Knev <kneav@students.softuni.bg>
2021-01-26 16:56:21 +0200

    EX_3

---
+++ Lab_DOM and Events/03. Delete-From-Table/app.js
@@ -0,0 +1,23 @@
+function deleteByEmail() {
+    let input = document.getElementsByTagName('input')[0].value.toLowerCase();
+    if (input.trim() === '') {
+        return;
+    }
+    let arr = Array.from(document.querySelectorAll('tbody > tr'));
+    let isDeleted=false;
+    for (let row of arr) {
+        row = Array.from(row.children);
+        let cellEmail = row[1];
+        if (cellEmail.textContent.toLowerCase().includes(input)) {
+        cellEmail.parentElement.remove();
+            document.getElementById('result').textContent='Deleted.'
+            isDeleted=true;   
+        }
+    }
+    if(!isDeleted){
+        document.getElementById('result').textContent='Not found.';
+    }
+
+
+  //  document.getElementsByTagName('input')[0].value = '';
+}
