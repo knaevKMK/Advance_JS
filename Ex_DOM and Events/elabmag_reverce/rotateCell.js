7eb86ebe64ed8de04248b9fdae4e2b8c17ec01cf
Kr_Knev <kneav@students.softuni.bg>
2021-01-28 01:53:39 +0200

    add 6th element
    add

--- Ex_DOM and Events/elabmag_reverce/rotateCell.js
+++ Ex_DOM and Events/elabmag_reverce/rotateCell.js
@@ -14,9 +14,13 @@ imgArray[3].src = "png/orange.png";
 imgArray[4] = new Image;
 imgArray[4].src = "png/sliva.png";
 
+imgArray[5] = new Image;
+imgArray[5].src = "png/star.png";
+
 imgArray.forEach(img => img.style = "width:150px;height:150px;")
 
 
+var autoPlayOn = false;
 
 function getImage() {
     console.log('I am Play')
@@ -24,13 +28,20 @@ function getImage() {
     for (let row of rows) {
         row = [...row.children];
         for (let cell of row) {
-            let rnd = Math.abs(Math.floor(Math.random() * 5));
-            console.log(rnd);
-            cell.children[0].src = imgArray[rnd].src;
-
+            let rnd = Math.abs(Math.floor(Math.random() * 6));
+            let pos = cell.children[0];
+            pos.src = imgArray[rnd].src;
+            // need animate
         }
         console.log(`=========================================`)
     }
+}
 
-
+function Autoplay() {
+    console.log('Auto PLay');
+    autoPlayOn = autoPlayOn ? false : true;
+    //  while (autoPlayOn) {
+    // hold time & eventButton stopAutoPlay
+    getImage();
+    //  }
 }
