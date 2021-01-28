17955639a584ec6533acb935a2d8547346aff8ad
Kr_Knev <kneav@students.softuni.bg>
2021-01-28 12:06:39 +0200

    Update solution.js

--- Ex_DOM and Events/02. Time-Converter/solution.js
+++ Ex_DOM and Events/02. Time-Converter/solution.js
@@ -1,4 +1,36 @@
 function attachEventsListeners() {
+    let inDay = document.getElementById('days');
+    let inHour = document.getElementById('hours');
+    let inMin = document.getElementById('minutes');
+    let inSec = document.getElementById('seconds');
 
-    console.log('TODO:...');
+    const btnDay = document.getElementById('daysBtn');
+    const btnHours = document.getElementById('hoursBtn');
+    const btnMin = document.getElementById('minutesBtn');
+    const btnSec = document.getElementById('secondsBtn');
+
+    btnDay.addEventListener('click', function() {
+        let value = Number(inDay.value);
+        inHour.value = value * 24;
+        inMin.value = value * 24 * 60;
+        inSec.value = value * 24 * 60 * 60;
+    });
+    btnHours.addEventListener('click', function() {
+        let value = Number(inHour.value);
+        inDay.value = value / 24;
+        inMin.value = value * 60;
+        inSec.value = value * 60 * 60;
+    });
+    btnMin.addEventListener('click', function() {
+        let value = Number(inMin.value);
+        inDay.value = value / 60 / 24;
+        inHour.value = value / 60;
+        inSec.value = value * 60;
+    });
+    btnSec.addEventListener('click', function() {
+        let value = Number(inSec.value);
+        inDay.value = value / 24 / 60 / 60;
+        inHour.value = value / 60 / 60;
+        inMin.value = value / 60;
+    });
 }
