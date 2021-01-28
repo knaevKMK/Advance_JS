829ac4f1292cf047392fba88774c9ed3a0a114e5
Kr_Knev <kneav@students.softuni.bg>
2021-01-28 14:52:00 +0200

    Update dropdown.js

--- Ex_DOM and Events/04. Fill-Dropdown/dropdown.js
+++ Ex_DOM and Events/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+
+    let text = document.getElementById('newItemText');
+    let value = document.getElementById('newItemValue');
+
+
+    let p = document.createElement('option');
+    p.textContent = text.value + ': ' + value.value;
+
+    document.getElementById('menu').appendChild(p);
+
+
+    text.value = '';
+    value.value = '';
 }
