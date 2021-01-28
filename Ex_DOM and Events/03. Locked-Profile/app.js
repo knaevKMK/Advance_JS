cde7ba58750377a1661b4a0f03a0fe8ee33d7d9a
Kr_Knev <kneav@students.softuni.bg>
2021-01-28 14:24:39 +0200

    Update app.js

--- Ex_DOM and Events/03. Locked-Profile/app.js
+++ Ex_DOM and Events/03. Locked-Profile/app.js
@@ -1,3 +1,16 @@
 function lockedProfile() {
-    console.log('TODO...')
+    document.getElementById('main').addEventListener('click', (e) => {
+        if (e.target.tagName == 'BUTTON') {
+            let currentProfile = e.target.parentNode;
+            if (currentProfile.querySelectorAll('input')[0].checked) {
+                return;
+            }
+            let hiddentext = currentProfile.querySelector('div');
+
+            let isVisible = hiddentext.style.display === "block"
+            hiddentext.style.display = isVisible ? 'none' : 'block ';
+
+            e.target.textContent = isVisible ? 'Show more' : 'Hide it';
+        }
+    });
 }
