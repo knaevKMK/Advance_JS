0fa265df33a0181d71948662935be14340dee4dd
Kr_Knev <kneav@students.softuni.bg>
2021-02-03 22:21:46 +0200

    Update app.js

--- Ex_Unit Testing and Error Handling/06.Dynamic-Validation/app.js
+++ Ex_Unit Testing and Error Handling/06.Dynamic-Validation/app.js
@@ -1,3 +1,14 @@
 function validate() {
-    // TODO
+    let input = document.getElementById('email');
+    input.addEventListener('keydown', function() {
+        let regex = /a-z]+@[a-z]+.[a-z]+/;
+        input.classList.add();
+        if (input.value.match(regex)) {
+            console.log(input.className);
+            input.className = '';
+        } else {
+            input.className = 'error';
+            console.log(input.className);
+        }
+    });
 }
