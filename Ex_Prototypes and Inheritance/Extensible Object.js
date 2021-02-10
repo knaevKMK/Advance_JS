5516b1dd2f2982862085f474ca54a90234770a79
Kr_Knev <kneav@students.softuni.bg>
2021-02-10 20:22:09 +0200

    Create Extensible Object.js

---
+++ Ex_Prototypes and Inheritance/Extensible Object.js
@@ -0,0 +1,24 @@
+function solve() {
+    const proto = {};
+    const inst = Object.create(proto);
+
+
+    inst.extend = function(templates) {
+        Object.entries(templates).forEach(([key, value]) => {
+            if (typeof value === 'function') {
+                proto[key] = value;
+            } else {
+                inst[key] = value;
+            }
+        });
+    }
+    return inst;
+ }
+// myObj: {
+//     __proto__: {}
+//     extend: function() {…}
+// }
+// template: {
+//     extensionMethod: function() {…},
+//     extensionProperty: 'someString'
+// }
