d79d666f9b0a1245bce7c9fde1de1bad320c2fcc
Kr_Knev <kneav@students.softuni.bg>
2021-02-10 23:31:22 +0200

    100/100

--- Ex_Prototypes and Inheritance/Computer.js
+++ Ex_Prototypes and Inheritance/Computer.js
@@ -1,10 +1,9 @@
-function createComputerHierarchy() {}
+function createComputerHierarchy() {
     class Component {
         constructor(manufacturer) {
             this.manufacturer = manufacturer;
         }
     }
-
     class Keyboard extends Component {
         constructor(...args) {
             super(args[0]);
@@ -27,13 +26,13 @@ function createComputerHierarchy() {}
         }
     }
 
-    class Computer  {
+    class Computer {
         constructor(...args) {
 
             if (new.target === Computer) {
                 throw new Error()
             }
-            this.manufacturer =args[0];
+            this.manufacturer = args[0];
             this.processorSpeed = Number(args[1]);
             this.ram = Number(args[2]);
             this.hardDiskSpace = Number(args[3]);
@@ -46,16 +45,16 @@ function createComputerHierarchy() {}
             super(args[0], args[1], args[2], args[3]);
             this.weight = Number(args[4]);
             this.color = args[5];
-            this.battery = this._setBattery(args[6]);
+            this.battery = (args[6]);
         }
 
-        get Battery() {
-            return this.battery;
+        get battery() {
+            return this._battery;
         }
 
-        _setBattery(args) {
+        set battery(args) {
             if (args instanceof Battery) {
-                return args;
+                return this._battery = args;
             }
             throw new TypeError('Invalid battery type')
         }
@@ -64,48 +63,40 @@ function createComputerHierarchy() {}
     class Desktop extends Computer {
         constructor(...args) {
             super(args[0], args[1], args[2], args[3]);
-            this.keyboard = this._setKeyboard(args[4]);
-            this.monitor = this._setMonitor(args[5]);
+            this.keyboard = (args[4]);
+            this.monitor = (args[5]);
         }
 
-        getMonitor() {
-            return this.monitor;
+        get monitor() {
+            return this._monitor;
         }
 
-        getKeyboard() {
-            return this.keyboard;
+        get keyboard() {
+            return this._keyboard;
         }
 
-        _setMonitor(args) {
+        set monitor(args) {
             if (args instanceof Monitor) {
-                return args;
+                return this._monitor = args;
             }
             throw  new TypeError('Invalid monitor format')
         }
 
-        _setKeyboard(args) {
+        set keyboard(args) {
             if (args instanceof Keyboard) {
-                return args;
+                return this._keyboard = args;
             }
             throw  new TypeError('Invalid keyboard format')
         }
     }
 
-//     return {
-//         Battery,
-//         Keyboard,
-//         Monitor,
-//         Computer,
-//         Laptop,
-//         Desktop
-//     }
-// }
+    return {
+        Battery,
+        Keyboard,
+        Monitor,
+        Computer,
+        Laptop,
+        Desktop
+    }
+}
 
-let keyboard = new Keyboard('Logitech',70);
-let monitor = new Monitor('Benq',28,18);
-let battery = new Battery('Energy',3);
-let laptop = new Laptop("Hewlett Packard",2.4,4,0.5,3.12,"Silver",battery);
-laptop.battery = "pesho";
-let desktop = new Desktop("JAR Computers",3.3,8,1,keyboard,monitor);
-desktop.keyboard = "gosho"
-desktop.monitor = "stamat"
