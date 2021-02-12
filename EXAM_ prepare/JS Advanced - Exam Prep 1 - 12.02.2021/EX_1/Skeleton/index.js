9743adb97ee23be2e55c64ce3575bf8fede0f499
Kr_Knev <kneav@students.softuni.bg>
2021-02-12 15:02:39 +0200

    Update index.js

--- EXAM_ prepare/JS Advanced - Exam Prep 1 - 12.02.2021/EX_1/Skeleton/index.js
+++ EXAM_ prepare/JS Advanced - Exam Prep 1 - 12.02.2021/EX_1/Skeleton/index.js
@@ -7,10 +7,8 @@ function solve() {
     let moduleSelect = admin.querySelector('select');
     let btnAdd = admin.querySelector('button');
 
-
-    //temp data work with map dof div module and array for li
+    // div`s to insert result
     let modules = trainings.querySelector('.modules');
-
 
 
     btnAdd.addEventListener('click', e => {
@@ -22,16 +20,16 @@ function solve() {
         });;
 
 
-
         if (chooseModule == 'SELECT MODULE...' || name.value == '' || date.value == '') { return; }
         let div;
         let ul;
 
-        for (let _div of modules.children) {
+        for (let _div of Array.from(modules.children)) {
+            let divChildren = Array.from(_div.children);
 
-            if (_div.childNodes[0].textContent.replace('-MODULE', '') === chooseModule) {
+            if (divChildren[0].textContent === chooseModule + '-MODULE') {
                 div = _div;
-                ul = _div.childNodes[1];
+                ul = divChildren[1];
                 break;
             }
         }
@@ -55,7 +53,7 @@ function solve() {
         li.className = 'flex';
 
         let h4 = document.createElement('h4');
-        h4.textContent = name.value.toUpperCase() + '-' + date.value.replace('T', ' - ').replace(/-/g, '/');
+        h4.textContent = name.value + ' - ' + date.value.replace(/-/g, '/').replace('T', ' - ');
         li.appendChild(h4);
 
         let btnDel = document.createElement('button');
@@ -71,28 +69,24 @@ function solve() {
 
         li.appendChild(btnDel);
 
-        ul.appendChild(li);
+        let liList = Array.from(ul.children)
+        liList.push(li);
+
+        ul.innerHTML = '';
+        liList.sort((a, b) => {
+                let second = b.children[0].textContent;
+                second = second.substring(second.indexOf('-'))
+                let first = a.children[0].textContent;
+                first = first.substring(first.indexOf('-'))
+                return first.localeCompare(second)
+
+            })
+            .forEach(li => ul.appendChild(li))
 
 
         name.value = '';
         date.value = '';
         moduleSelect.selectedIndex = 0
 
-
-        // test from judge ( work in event {}, but not work out of event in the function solve?????)
-        // let elements = {
-        //     form: document.getElementsByTagName('form')[0],
-        //     name: document.querySelector('input[name="lecture-name"]'),
-        //     date: document.querySelector('input[name="lecture-date"]'),
-        //     module: document.querySelector('select[name="lecture-module"]'),
-        //     addBtn: document.querySelector('form button'),
-        //     modulesDiv: document.querySelector('.modules'),
-        //     moduleList: () => Array.from(document.querySelectorAll('.module')),
-        //     listItems: () => Array.from(document.querySelectorAll('.flex')),
-        // }
-        // console.log(elements.moduleList().length)
-        // console.log(elements.moduleList()[0].children[0].textContent)
-        // console.log(elements.listItems()[0].children[0].textContent)
-        // console.log(elements.listItems()[0].children[1].className)
     })
 }
