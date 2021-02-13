3be5442f4d16783d2fdc3f63e2c3a99f2ba5fce9
Kr_Knev <kneav@students.softuni.bg>
2021-02-13 19:06:27 +0200

    Update distanceConverter.js

--- Ex_DOM and Events/07. Distance-Converter/distanceConverter.js
+++ Ex_DOM and Events/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,18 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    const convertor_To_One_Meter = {
+        km: 1000,
+        m: 1,
+        cm: 0.01,
+        mm: 0.001,
+        mi: 1609.34,
+        yrd: 0.9144,
+        ft: 0.3048,
+        in: 0.0254
+    };
+    let [input, btn, result] = document.querySelectorAll('input')
+    let [from, to] = document.querySelectorAll('select')
+    btn.addEventListener('click', e => {
+        result.value = convertor_To_One_Meter[from.value] * Number(input.value) /
+            convertor_To_One_Meter[to.value]
+    })
 }
