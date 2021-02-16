570c662dbb27fe263e219c8197711e3ce22f7b48
Kr_Knev <kneav@students.softuni.bg>
2021-02-16 19:15:07 +0200

    90/100

--- EXAM_ prepare/JS Advanced Retake Exam - 26 Apr 2018/EX_2 Subscription Card/Subscription Card.test.js
+++ EXAM_ prepare/JS Advanced Retake Exam - 26 Apr 2018/EX_2 Subscription Card/Subscription Card.test.js
@@ -6,6 +6,9 @@ describe('SubscriptionCard', function () {
     let firstName;
     let lastName;
     let ssn;
+    let line = '120';
+    let startDate = new Date('2018-04-22');
+    let endDate = new Date('2018-05-21');
     beforeEach(function () {
         firstName = 'Pesho';
         lastName = 'Petrov';
@@ -44,6 +47,14 @@ describe('SubscriptionCard', function () {
         it('should geter', function () {
             expect(check.isBlocked).equal(check._blocked);
         });
+        it('should geter', function () {
+            check.block()
+            expect(check.isBlocked).equal(true);
+        });
+        it('should geter', function () {
+            check.unblock()
+            expect(check.isBlocked).equal(false);
+        });
     });
     describe('addSubscription(line, startDate, endDate) ', function () {
         it('should addSubscr', function () {
@@ -52,17 +63,25 @@ describe('SubscriptionCard', function () {
             let endDate = new Date('2018-05-21');
 
             check.addSubscription(line, startDate, endDate);
-            expect(check._subscriptions.length).equal(1);
+            check.addSubscription('*', new Date('2018-05-25'), new Date('2018-06-24'));
+
+            expect(check._subscriptions.length).equal(2);
             expect(check._subscriptions[0]).deep.equal({line, startDate, endDate});
         });
     });
     describe('  isValid(line, date) ', function () {
         it('should  false if is NOT blocked', function () {
-          //  check._blocked = false;
+            check.addSubscription(line, startDate, endDate);
+            check.addSubscription('*', new Date('2018-05-25'), new Date('2018-06-24'));
+
+         check.block()
             expect(check.isValid('120', new Date('2018-04-22'))).false;
         });
         it('should  false if is NOT bloked', function () {
-            expect(check.isValid('120', new Date('2018-04-22'))).false;
+            check.addSubscription(line, startDate, endDate);
+            check.addSubscription('*', new Date('2018-05-25'), new Date('2018-06-24'));
+
+            expect(check.isValid('120', new Date('2018-03-22'))).false;
         });
 
         it('should  true if is bloked', function () {
